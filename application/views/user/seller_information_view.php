73e957c49525f20cbd60fbad9f2e315ea99e0f95
Vincent David Guerrero Peña <v.davidguerrero@gmail.com>
2014-03-27 20:42:33 -0400

    auto Commit

--- application/views/user/seller_information_view.php
+++ application/views/user/seller_information_view.php
@@ -98,7 +98,7 @@
                   echo "
                       
                     <tr class='active'>
-                        <td> <a href='/ad_controller/showAd/".$pendingAd->Unique_Car->VIN."/".$pendingAd->Flag."'>".$pendingAd->Unique_Car->Unique_Model->Brand." </a></td>
+                        <td>" .$pendingAd->Unique_Car->Unique_Model->Brand." </a></td>
                         <td>".$pendingAd->Unique_Car->Unique_Model->Model." ".$pendingAd->Unique_Car->Unique_Model->Body_Style." ".$pendingAd->Unique_Car->Unique_Model->Trim. "</td>
                         <td>".$pendingAd->Unique_Car->Unique_Model->Year."</td>
                         <td>".number_format($pendingAd->Price)."</td>
