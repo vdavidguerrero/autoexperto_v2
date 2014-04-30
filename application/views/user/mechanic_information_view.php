ad34221ac05f5f645aa910f39a0b7d0b6d0ee801
Vincent David Guerrero Peña <v.davidguerrero@gmail.com>
2014-04-30 12:09:59 -0400

    auto Commit

--- application/views/user/mechanic_information_view.php
+++ application/views/user/mechanic_information_view.php
@@ -58,7 +58,7 @@
                   echo "
                       
                     <tr class='active'>
-                        <td> <a'>".$pendingAd->Unique_Car->Unique_Model->Brand." </a></td>
+                        <td> <a href='/ad_controller/showAd/".$activeAd->Unique_Car->VIN."/".$activeAd->Flag."'>".$activeAd->Unique_Car->Unique_Model->Brand." </a></td>
                         <td>".$pendingAd->Unique_Car->Unique_Model->Model." ".$pendingAd->Unique_Car->Unique_Model->Body_Style." ".$pendingAd->Unique_Car->Unique_Model->Trim. "</td>
                         <td>".$pendingAd->Unique_Car->Unique_Model->Year."</td>
                         <td>".number_format($pendingAd->Price)."</td>
