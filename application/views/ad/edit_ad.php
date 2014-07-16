459b6952cc8b8578f45620bd478d9d035a11ae00
Vincent David Guerrero Peña <v.davidguerrero@gmail.com>
2014-07-16 16:33:37 -0400

    auto Commit

--- application/views/ad/edit_ad.php
+++ application/views/ad/edit_ad.php
@@ -17,29 +17,29 @@
 
 <?php
     if($statitics == 1){
-        echo "<h4>Precios sugerido para que el vehículo se venda </h2><br>";
-        echo "En 1 Dia ";
+        echo "<h4><b>Precios sugerido para que el vehículo se venda</b> </h2><br>";
+        echo "<b>En 1 Dia</b> ";
         echo number_format(round($first));
         echo "<br>";
 
-        echo "En 15 Dias ";
+        echo "<b>En 15 Dias</b> ";
         echo number_format(round($second));
         echo "<br>";
 
-        echo "En 30 Dias ";
+        echo "<b>En 30 Dias</b> ";
         echo number_format(round($third));
         echo "<br>";
 
-        echo "En 45 Dias ";
+        echo "<b>En 45 Dias</b> ";
         echo number_format(round($fourth));
         echo "<br>";
-        echo "<h4> Cambiar Precio?</h4><br>";
+        echo "<h4><b> Cambiar Precio?</b></h4><br>";
         echo "<input type='text' name='price' value='".$ad->Price."'><br>";
 
     }
     else
     {
-        echo "<h4 class='text-danger'>No hay suficientes datos para estimar un precio</h4>
+        echo "<h4 class='text-danger'><b>No hay suficientes datos para estimar un precio</b></h4>
         <br>";
         echo "<input type='hidden' name='price' value='".$ad->Price."'><br>";
         echo "<br>";
