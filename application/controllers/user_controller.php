b75623833542595a36072326c4c11762b301b227
Vincent David Guerrero Peña <v.davidguerrero@gmail.com>
2014-07-16 13:38:53 -0400

    auto Commit

--- application/controllers/user_controller.php
+++ application/controllers/user_controller.php
@@ -111,7 +111,7 @@ class user_controller extends Main_Controller {
             $sessionInfo = $this->session->userdata('logged_in');
             $id     = $sessionInfo['id'];
             $flag   = $sessionInfo['flag'];
-            $datapass["flag"] = $flag;
+            $dataPass["flag"] = $flag;
             if($id)
             {
                 $dataPass["user"]       =  $this->user_model->getUser($id);
